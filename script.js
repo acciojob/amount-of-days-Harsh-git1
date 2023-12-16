93a0a2ff5efb487b5c7010d51091cfacd981f1bb
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-16 07:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function daysOfAYear(y) {
+	if(y < 1 || y>9999) return;
+	
+	if (y%100 === 0) {
+		if(y%400 === 0) return 366;
+		else return 365;
+	}else {
+		if(y%2 === 0) {
+		return 366;
+		}else {
+			return 365;
+		}
+	} 
+}
+// alert(daysOfAYear(99999))
